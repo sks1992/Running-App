ed7e424c28c7cb54e02ff28d42c1895d458fcd44
sandeep <sksandeep9215@gmail.com>
2021-06-10 18:13:44 +0530

    navHost And Menu Setting

--- app/src/main/java/sandeep/kumar/runningapp/MainActivity.kt
+++ app/src/main/java/sandeep/kumar/runningapp/MainActivity.kt
@@ -5,8 +5,10 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/sandeep/kumar/runningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/sandeep/kumar/runningapp/ui/fragments/RunFragment.kt
@@ -1,8 +1,12 @@
 package sandeep.kumar.runningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 import sandeep.kumar.runningapp.R
 import sandeep.kumar.runningapp.ui.viewmodels.MainViewModel
 
@@ -11,5 +15,13 @@ import sandeep.kumar.runningapp.ui.viewmodels.MainViewModel
 class RunFragment : Fragment(R.layout.fragment_run) {
 
     private val viewModel:MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 
 }
