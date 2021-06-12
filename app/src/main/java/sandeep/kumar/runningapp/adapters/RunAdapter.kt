cfedb176df49f77780b826e9052bac4f538fd0b5
sandeep <sksandeep9215@gmail.com>
2021-06-12 17:21:33 +0530

    Create RecyclerViewAdapter

--- app/src/main/java/sandeep/kumar/runningapp/adapters/RunAdapter.kt
+++ app/src/main/java/sandeep/kumar/runningapp/adapters/RunAdapter.kt
@@ -1,4 +1,75 @@
 package sandeep.kumar.runningapp.adapters
 
-class RunAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_run.view.*
+import sandeep.kumar.runningapp.R
+import sandeep.kumar.runningapp.db.Run
+import sandeep.kumar.runningapp.util.TrackingUtility
+import java.text.SimpleDateFormat
+import java.util.*
+
+class RunAdapter :RecyclerView.Adapter<RunAdapter.RunViewHolder>() {
+
+    inner class RunViewHolder(itemView:View):RecyclerView.ViewHolder(itemView)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RunViewHolder {
+        return RunViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_run,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RunViewHolder, position: Int) {
+        val run = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(run.image).into(ivRunImage)
+
+            val calender =Calendar.getInstance().apply {
+                timeInMillis  = run.timestamp
+            }
+            val dateFormat = SimpleDateFormat("dd.MM.yy",Locale.getDefault())
+            tvDate.text = dateFormat.format(calender.time)
+
+            val avgSpeed = "${run.averageSpeed} km/h"
+            tvAvgSpeed.text =avgSpeed
+
+            val distanceInKm = "${run.distanceInMeters /1000f}km"
+            tvDistance.text =distanceInKm
+
+
+            tvTime.text =TrackingUtility.getFormattedStopWatchTime(run.timeInMillis)
+
+            val caloriesBurned ="${run.caloriesBurned}Kcal"
+            tvCalories.text = caloriesBurned
+        }
+    }
+
+    override fun getItemCount(): Int =differ.currentList.size
+
+
+    val diffCallback = object : DiffUtil.ItemCallback<Run>() {
+        override fun areItemsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return oldItem.id == newItem.id
+         }
+
+        override fun areContentsTheSame(oldItem: Run, newItem: Run): Boolean {
+
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+
+    }
+
+    val differ =AsyncListDiffer(this,diffCallback)
+
+    fun submitList(list:List<Run>) = differ.submitList(list)
 }
--- app/src/main/java/sandeep/kumar/runningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/sandeep/kumar/runningapp/ui/fragments/RunFragment.kt
@@ -6,12 +6,15 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_run.*
 import pub.devrel.easypermissions.AppSettingsDialog
 import pub.devrel.easypermissions.EasyPermissions
 import sandeep.kumar.runningapp.R
+import sandeep.kumar.runningapp.adapters.RunAdapter
 import sandeep.kumar.runningapp.ui.viewmodels.MainViewModel
 import sandeep.kumar.runningapp.util.Constants.REQUEST_CODE_LOCATION_PERMISSION
 import sandeep.kumar.runningapp.util.TrackingUtility
@@ -22,14 +25,28 @@ class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionC
 
     private val viewModel: MainViewModel by viewModels()
 
+    private lateinit var runAdapter: RunAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         requestPermissions()
 
+        setUpRecyclerView()
+
+        viewModel.runsSortedByDate.observe(viewLifecycleOwner, Observer {
+            runAdapter.submitList(it)
+        })
         fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
+    }
+
+
+    private fun setUpRecyclerView() = rvRuns.apply {
+        runAdapter = RunAdapter()
+        adapter = runAdapter
+        layoutManager = LinearLayoutManager(requireContext())
     }
 
     private fun requestPermissions() {
